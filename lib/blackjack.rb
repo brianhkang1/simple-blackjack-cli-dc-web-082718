e11edae877a458ba0caf3da8a17b03899fd2fb95
Brian Kang <brianhkang1@gmail.com>
2018-08-07 23:38:56 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,7 +23,10 @@ def end_game(card_total)
 end
 
 def initial_round
-  # code #initial_round here
+  sum1 = deal_card
+  sum2 = deal_card
+  total_sum = sum1 + sum2
+  display_card_total
 end
 
 def hit?

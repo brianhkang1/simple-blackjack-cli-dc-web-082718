511285df2fb6d9236f37febdebe788b46f08afb4
Brian Kang <brianhkang1@gmail.com>
2018-08-08 00:08:41 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,6 +53,10 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome 
+  initial_round 
+  until card_total >= 21
+  hit?(card_total)
+  
 end
     

85563e142618824d6bd6b79a0ed8251bf64de978
Brian Kang <brianhkang1@gmail.com>
2018-08-08 00:16:19 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -56,9 +56,9 @@ def runner
   welcome 
   initial_round 
   until card_total > 21
-    hit?
-    display_card_total
+    hit?(card_total)
+    display_card_total(card_total)
   end
-  end_game
+  end_game(card_total)
 end
     
